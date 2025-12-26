105e3e8327e121770056bd6d332609d65c5183c5
Raksa <novraksa204@gmail.com>
2025-12-26 10:14:55 +0700

    prepare backend for vercel

--- src/server.js
+++ src/server.js
@@ -3,12 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-console.log("🚀 Starting server...");
+// Connect DB ONCE (serverless-safe)
+connectDB();
 
-connectDB(); // connect to MongoDB
-
-const PORT = process.env.PORT || 3200;
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+module.exports = app;
